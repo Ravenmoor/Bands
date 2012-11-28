a7bf1babe70e62a9df6058cd959e61eeace4f8bd
Michael Ferrell <michael.d.ferrell@gmail.com>
2012-11-28 16:31:51 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bands::Application.config.secret_token = 'db3ebb0351d513d0e083468b2de784fa09d902d58cbf84e845ea371d88c11178e267058cd54884e9e7aaefa995b05226383f40481deadcd8dfeaf79c17b7745d'
